74d354ac41c23a84b27e5f30478e2309244db6ef
Rakesh kumar <rakesh.bahel@gmail.com>
2024-07-30 12:05:12 +0530

    Save

--- HangMan/src/components/Button/Button.jsx
+++ HangMan/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ function Button(){
     return (
         <React.Fragment>
         <button className="border-2 border-red-500 px-2 w-38 bg-slate-100 hover:bg-yellow-300 rounded-lg" >Show the entered text</button>
-        <button id="btn" className=" border-solid border-2 border-sky-500  px-2 w-28 bg-slate-100 hover:bg-sky-700  rounded-lg">Submit !</button>
+        <button id="btn" className=" border-solid border-2 border-sky-500  px-2 w-28 bg-slate-100 hover:bg-sky-700 hover:text-white rounded-lg">Submit !</button>
         </React.Fragment>
     );
 }
