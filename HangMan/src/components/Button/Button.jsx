a4a4a748c1aaff24f896ca78b4f79c0b7df5e862
Rakesh kumar <rakesh.bahel@gmail.com>
2024-07-30 11:53:49 +0530

    Save

--- HangMan/src/components/Button/Button.jsx
+++ HangMan/src/components/Button/Button.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Button(){
     return (
         <React.Fragment>
-        <button className="border-2 border-red-500 px-2 w-38 bg-slate-100" >Show the entered text</button>
-        <button id="btn" className=" border-solid border-2 border-sky-500  px-2 w-28 bg-slate-100 hover:bg-sky-700  ">Submit !</button>
+        <button className="border-2 border-red-500 px-2 w-38 bg-slate-100 hover:bg-yellow-300 rounded-lg" >Show the entered text</button>
+        <button id="btn" className=" border-solid border-2 border-sky-500  px-2 w-28 bg-slate-100 hover:bg-sky-700  rounded-lg">Submit !</button>
         </React.Fragment>
     );
 }
--- HangMan/src/components/InputText/InputText.jsx
+++ HangMan/src/components/InputText/InputText.jsx
@@ -3,9 +3,9 @@ import Button from "../Button/Button";
 function InputText(){
     return (
         
-        <div className="flex bg-slate-200 mx-6 px-8 space-x-40  justify-center">
-            <label className="flex font-bold py-3"> Enter Suggested word</label>
-            <input className=" border-2 border-red-500 py-3 " type="password" placeholder="Please Enter Text" />
+        <div className="lg:file:flex md:block bg-slate-200 mx-6 px-8 py-4 space-x-40  justify-center">
+            <label className="md:text-center font-bold py-3 "> Enter Suggested word</label>
+            <input className=" border-2 border-red-500 py-3 rounded-lg " type="password" placeholder="Please Enter Text" />
             <Button/>
         
         </div>
